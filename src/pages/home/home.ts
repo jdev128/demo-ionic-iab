489c994fae66d71ef34d602ba26f9465058403d5
Mauricio Pisco <mauriciopisco@gmail.com>
2021-03-31 10:21:52 -0300

    Integrar Cordova InAppBrowser en la aplicacion

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -7,8 +8,25 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  params= "";
+  private browser;
 
+  constructor(public navCtrl: NavController, public iab: InAppBrowser) {
+
+  }
+
+  openBrowser() {
+    this.browser = this.iab.create("http://192.168.0.4:8100/tabs/inicio"+this.params, "_blank", "location=no");
+    this.browser.on("message").subscribe(message => {
+      if (message.data && message.data) {
+          if (message.data.event === "close") {
+            this.browser.close();
+          } else if (message.data.event === "phone") {
+            window.open("tel:" + message.data.data.number, "_system");
+          } else if (message.data.event === "whatsapp") {
+            window.open("whatsapp://send?phone=" + message.data.data.number, "_system");
+          }
+    }});
   }
 
 }

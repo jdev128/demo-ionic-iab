a7cdba43bb44c7bb207368959a75b6cde2d5caf1
Mauricio Pisco <mauriciopisco@gmail.com>
2021-10-29 18:38:01 -0300

    Editar Pagina Raiz

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,7 +8,13 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  params= "";
+  environment: string;
+
+  devURL = 'https://app-socios-frontend-jmmsnlcpta-uc.a.run.app';
+  localURL = 'http://192.168.0.1:8100';
+
+  params: string;
+
   private browser;
 
   constructor(public navCtrl: NavController, public iab: InAppBrowser) {
@@ -16,7 +22,9 @@ export class HomePage {
   }
 
   openBrowser() {
-    this.browser = this.iab.create("http://192.168.0.4:8100/tabs/inicio"+this.params, "_blank", "location=no");
+    let host = this.environment === 'dev'? this.devURL : this.localURL;
+    let url = this.params? host + "/tabs/inicio?" + this.params : host;
+    this.browser = this.iab.create(url, "_blank", "location=no");
     this.browser.on("message").subscribe(message => {
       if (message.data && message.data) {
           if (message.data.event === "close") {
@@ -24,7 +32,7 @@ export class HomePage {
           } else if (message.data.event === "phone") {
             window.open("tel:" + message.data.data.number, "_system");
           } else if (message.data.event === "whatsapp") {
-            window.open("whatsapp://send?phone=" + message.data.data.number, "_system");
+            window.open("whatsapp://send?phone=" + message.data.data.number);
           }
     }});
   }

f148bc3c54291645859e1a9ec9e24ec97ec87d1c
Mauricio Pisco <mauriciopisco@gmail.com>
2021-11-09 18:36:02 -0300

    Corregir enlaces en IOS

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,6 +12,7 @@ export class HomePage {
 
   devURL = 'https://app-socios-frontend-jmmsnlcpta-uc.a.run.app';
   testURL = 'https://app-socios-frontend-zrvedpo66q-uc.a.run.app/';
+  prodURL = 'https://asistenciamedica.osde.com.ar/';
   localURL = 'http://192.168.0.1:8100';
 
   params: string;
@@ -24,23 +25,29 @@ export class HomePage {
 
   openBrowser() {
     let host;
-    if (this.environment === 'dev') {
-      host = this.devURL;
-    } else if (this.environment === 'test') {
-      host = this.testURL;
-    } else {
-      host = this.localURL;
+    switch (this.environment) {
+      case 'dev':
+        host = this.devURL;
+        break;
+      case 'test':
+        host = this.testURL;
+        break;
+      case 'prod':
+        host = this.prodURL;
+        break;
+      default:
+        host = this.localURL
     }
-    let url = this.params? host + "/tabs/inicio?" + this.params : host;
+    let url = this.params? host + "/tabs/inicio?code=" + this.params : host;
     this.browser = this.iab.create(url, "_blank", "location=no");
     this.browser.on("message").subscribe(message => {
       if (message.data && message.data) {
           if (message.data.event === "close") {
             this.browser.close();
           } else if (message.data.event === "phone") {
-            window.open("tel:" + message.data.data.number, "_system");
+            window.open("tel:" + message.data.data.number, "_self");
           } else if (message.data.event === "whatsapp") {
-            window.open("whatsapp://send?phone=" + message.data.data.number);
+            window.open("https://wa.me/" + message.data.data.number, '_self');
           }
     }});
   }
